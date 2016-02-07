f250f4cd5509dc6af044afb876b9b94a726ecacc
marict <marictdude@yahoo.com>
2016-02-07 12:49:12 -0800

    Added comments to Instructions

--- src/gcode-analyzer/GCode.java
+++ src/gcode-analyzer/GCode.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
-// Nice work on spacing
-
+// Parent class of all GCode subclasses. Contains arraylist of commands.
 public class GCode {
   protected List<String> commands;
   protected String gcodeFileName;
--- src/gcode-analyzer/Instructions.java
+++ src/gcode-analyzer/Instructions.java
@@ -1,40 +1,56 @@
+/*
+Author: Melissa Medsker-Galloway, Paul Curry
+PLSE Incarnate Project
+Instructions class which contains the instructions in a gCode file as opposed to the preamble and end code.
+
+Current features:
+  Loading in instructions, printing out instructions to text file.
+*/
+
 import java.util.*;
 import java.io.*;
 
+ 
 public class Instructions extends GCode {
 
   public Instructions() {
     super();
   }
-
-  public void load(String	fileName) throws IOException {
-    File f	= new	File(fileName);
+  
+  // Pre: file nam exists
+  // Fills the instructions object's commands with instruction commands from instruction part 
+  // of the gCode file.
+  public void load(String fileName) throws IOException {
+    File f  = new File(fileName);
     this.gcodeFileName = fileName;
     Scanner seeker = new Scanner(f);
-    String	line = "";
-    boolean start =	false;
+    String  line = "";
+    boolean start = false;
     while (!start) {
- 	    if (line.indexOf("end of start.gcode") != -1) {
- 		    start = true;
- 	    }
- 	    line = seeker.nextLine();
+      if (line.indexOf("end of start.gcode") != -1) {
+        start = true;
+      }
+      line = seeker.nextLine();
     }
     while (seeker.hasNextLine()) {
       this.commands.add(line);
       line = seeker.nextLine();
     }
   }
-
+  
+  // Post: writes this Instructions's commands to a new txt file with -i added to source gcode
   public void write() throws IOException {
     String outputFileName = this.instrName();
-    PrintWriter writer = new	PrintWriter(outputFileName, "UTF-8");
-    for	(String command :	this.commands)	{
-   	  writer.println(command);
+    PrintWriter writer = new  PrintWriter(outputFileName, "UTF-8");
+    for (String command : this.commands)  {
+      writer.println(command);
     }
     writer.close();
   }
 
+   // returns txt file name in the format of "sourceFileName-i.txt"
   public String instrName() {
-    return gcodeFileName.substring(0, gcodeFileName.lastIndexOf("."))	+ "-i.txt";
+    return gcodeFileName.substring(0, gcodeFileName.lastIndexOf(".")) + "-i.txt";
   }
+  
 }
