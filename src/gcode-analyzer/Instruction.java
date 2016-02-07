c63bc8af1134e27f747a405849e188c553f478b1
marict <marictdude@yahoo.com>
2016-02-07 13:09:17 -0800

    Added instruction.java file

---
+++ src/gcode-analyzer/Instruction.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+// Parent class of all instruction subclasses. Contains the text of a command.
+public class Instruction {
+  protected text
+
+  public Instruction(String text) {
+    this.text = text;
+  }
+}
