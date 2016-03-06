e30bd6d55e95ed04e3f76cd3bdc164292ab00cca
Melissa Medsker-Galloway <medskm@uw.edu>
2016-03-06 13:40:37 -0800

    Added midair print checks with ZLevel class

---
+++ src/gcode-analyzer/0203/src/ZLevel.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class ZLevel {
+	private double zindex;
+	private Map<Double, Set<Double>> xmap;
+	
+	public ZLevel(double zindex) {
+		this.zindex = zindex;
+		this.xmap = new TreeMap<Double, TreeSet<Double>>();
+	}
+	
+	public void add(double x, double y) {
+		if (this.xmap(!containsKey(x)) {
+			this.xmap.put(x, new TreeSet<Double>());
+		}
+		this.xmap.get(x).add(y);
+	}
+	
+	public double getZIndex() {
+		return this.zindex;
+	}
+	
+	public Set<Double> getXValues() {
+		return this.xmap.keySet();
+	}
+	
+	public Set<Double> getYValues(double x) {
+		if (!this.xmap.containsKey(x)) {
+			return null;
+		} else {
+			return this.xmap.get(x);
+		}
+	}
+	
+	public void printZLevel() {
+		System.out.println("Z index: " + this.zindex);
+		for(double x : this.xmap.keySet()) {
+			System.out.println("\tX coordinate: " + x);
+			for (double y : this.xmap.get(x)) {
+				System.out.println("\t\tY coordinate: " + y);
+			}
+		}
+	}
+}
