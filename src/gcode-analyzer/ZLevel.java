dbadc76b8ba97b98ce22e3daf3e62115da9c0989
Melissa Medsker-Galloway <medskm@uw.edu>
2016-03-06 16:01:08 -0800

    remove unnecessary whitespace

--- src/gcode-analyzer/ZLevel.java
+++ src/gcode-analyzer/ZLevel.java
@@ -3,27 +3,27 @@ import java.util.*;
 public class ZLevel {
 	private double zindex;
 	private Map<Double, Set<Double>> xmap;
-	
+
 	public ZLevel(double zindex) {
 		this.zindex = zindex;
 		this.xmap = new TreeMap<Double, TreeSet<Double>>();
 	}
-	
+
 	public void add(double x, double y) {
 		if (this.xmap(!containsKey(x)) {
 			this.xmap.put(x, new TreeSet<Double>());
 		}
 		this.xmap.get(x).add(y);
 	}
-	
+
 	public double getZIndex() {
 		return this.zindex;
 	}
-	
+
 	public Set<Double> getXValues() {
 		return this.xmap.keySet();
 	}
-	
+
 	public Set<Double> getYValues(double x) {
 		if (!this.xmap.containsKey(x)) {
 			return null;
@@ -31,7 +31,7 @@ public class ZLevel {
 			return this.xmap.get(x);
 		}
 	}
-	
+
 	public void printZLevel() {
 		System.out.println("Z index: " + this.zindex);
 		for(double x : this.xmap.keySet()) {
