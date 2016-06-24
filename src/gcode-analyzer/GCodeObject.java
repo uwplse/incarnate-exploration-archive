e6b2c1e2abc8cee395343e274ae763a731c728a0
Melissa Medsker-Galloway <medskm@cs.washington.edu>
2016-06-24 11:54:47 -0700

    added anlayzer method stubs to GCodeObject

--- src/gcode-analyzer/GCodeObject.java
+++ src/gcode-analyzer/GCodeObject.java
@@ -69,4 +69,51 @@ public class GCodeObject {
     // System.out.println("Command stats for EndCode:");
     // this.endCode.process();
 	}
+
+
+   // returns layer height(s) (mm) of the GCode file
+   // Cura docs: "The layer height directly affects the quality of your
+   // final print. The default of .2mm gives decent prints, but for high
+   // quality prints is also possible to lower layer height to .1mm (note
+   // that this will double the building time.)"
+   public double getLayerHeight() { return 0.0; }
+
+   // returns shell thickness (mm) of the outside walls.
+   // Cura docs: "Normal thickness of 0.8mm (2 perimeters) gives good results, but for
+   // small prints it's sometimes better to use 0.4mm (1 perimeter)."
+   public double getShellThickness() { return 0.0; }
+
+   // returns the bottom/top thickness which controls how many layers are
+   // fully filed.
+   // Cura docs: "Setting this higher gives a stronger part and setting it
+   // lower gives a weaker part (which prints faster). In general, 0.6mm
+   // gives strong parts without holes and going lower can cause holes
+   // to appear at the top of your models."
+   public double getBottomTopThickness() { return 0.0; }
+
+   // returns layer thickness of bottom layer in mm units. A thicker first
+   // layer improves adhesion to bed. If 0, is the same layer height as
+   // others.
+   public double getIniitalLayerThickness() { return 0.0; }
+
+   public double fillDensity() { return 0.0; }
+
+   // returns the extruding speed of the printer head in mm/s units.
+   public double getPrintingSpeed() { return 0.0; }
+
+   // returns speed that the nozzle has when filling the perimeter
+   public double getInfillSpeed() { return 0.0; }
+
+   // returns printing temperature in degrees Celsius
+   public double getPrintingTemperature() { return 0.0; }
+
+   public double getFilamentDiameter() { return 0.0; }
+
+   public double getFlowRate() { return 0.0; }
+
+   // returns minimum time spent in a layer, which gives the layer time to
+   // cool down until the next layer is printed. A higher value slows down
+   // the printing speed of small layers.
+   public double getMinimalLayerTime() { return 0.0; }
 }
+
