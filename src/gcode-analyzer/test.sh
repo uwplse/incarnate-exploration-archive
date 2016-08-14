be4de177189034f0ef6595543de825b558638fd4
Zach Tatlock <ztatlock@cs.washington.edu>
2016-08-14 15:54:03 -0700

    colors for test script

--- src/gcode-analyzer/test.sh
+++ src/gcode-analyzer/test.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env bash
 
-# NOTE! assumes user runs from this dir
+D="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for g in $(find ../.. -name '*.gcode'); do
-  printf "Analyzing %s\n" "$g"
-  java GCodeAnalyzer "$g"
-  echo
+PASS="PASS"
+FAIL="FAIL"
+
+# ANSI color codes
+BK=$'\033[1;30m' # black
+RD=$'\033[1;31m' # red
+GR=$'\033[1;32m' # green
+YL=$'\033[1;33m' # yellow
+BL=$'\033[1;34m' # blue
+MG=$'\033[1;35m' # magenta
+CY=$'\033[1;36m' # cyan
+WT=$'\033[1;37m' # white
+NC=$'\033[0m'    # no color
+
+# only output color for ttys
+if [ -t 1 ]; then
+  PASS="${GR}PASS${NC}"
+  FAIL="${RD}FAIL${NC}"
+fi
+
+for g in $(find ${D}/../.. -name '*.gcode'); do
+  printf "%-45s" "$(basename "$g")"
+  if java GCodeAnalyzer "$g" > /dev/null 2>&1; then
+    echo "$PASS"
+  else
+    echo "$FAIL"
+  fi
 done
