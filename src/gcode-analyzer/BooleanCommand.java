71ad50ebbfa43af1322ee5078b2a9b720d385ed4
medskm <medskm@cs.washington.edu>
2016-08-14 15:59:56 -0700

    Added documentation to new Command and BooleanCommand files

--- src/gcode-analyzer/BooleanCommand.java
+++ src/gcode-analyzer/BooleanCommand.java
@@ -1,4 +1,11 @@
+/* The BooleanCommand class represents a G-Code Command which has both a code value
+ * (e.g., "G1" or "M20"), line number, but no parameters. The presence of a
+ * BooleanCommand in a G-Code file indicates that the Command is "set" (e.g.,
+ * G21 to indicate units are set to mm in the print). */
 public class BooleanCommand extends Command {
+
+  /* Constructs a BooleanCommand with given code and line number associated
+   * with source file */
   public BooleanCommand(String code, int lineNumber) {
     super(code, lineNumber);
   }
--- src/gcode-analyzer/Command.java
+++ src/gcode-analyzer/Command.java
@@ -1,12 +1,17 @@
+/* The Command class represents a G-Code Command which has both a code value
+ * (e.g., "G1" or "M20"), line number */
 public class Command implements Comparable<Command> {
   private String code;
   private int lineNumber;
 
+  /* Constructs a Command object with given code (e.g., "G1") and
+   * line number from a  source file */
   public Command(String code, int lineNumber) {
     this.code = code;
     this.lineNumber = lineNumber;
   }
 
+  /* Returns line number associated with this Command from the source file */
   public int getLineNumber() {
     return this.lineNumber;
   }
@@ -16,6 +21,7 @@ public class Command implements Comparable<Command> {
     return this.code;
   }
 
+  /* Commands are compared by line number in ascending order */
   public int compareTo(Command other) {
     return this.getLineNumber() - other.getLineNumber();
   }
